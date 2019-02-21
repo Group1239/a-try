40529ce2b5a661273d8918a9fec46b45ba2d2119
Group1239 <47778352+Group1239@users.noreply.github.com>
2019-02-21 09:31:49 +0100

    Uploading the simple Cat class again
    With an IAnimal interface

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public interface IAnimal
+    {
+        string GetDescription();
+    }
+
+    class Cat : IAnimal
+    {
+        public string GetDescription()
+        {
+            return "I'm a cat";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+
+            Console.WriteLine(myCat.GetDescription());
+        }
+    }
+
+}
