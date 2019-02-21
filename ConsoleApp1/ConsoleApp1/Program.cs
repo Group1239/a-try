0fcadb7160d74ea75fe07d1fd2d0c601927adc87
Lukis543 <lukis543@gmail.com>
2019-02-21 09:20:59 +0100

    Coded

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class New_Interface
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+            int z = y * x;
+            Console.WriteLine (z);
+            Console.ReadLine();
+        }
+    }
+}
