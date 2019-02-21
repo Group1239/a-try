4a73529fb6e9ac37d8a2bad86610161d35fad6c1
Group1239 <47778352+Group1239@users.noreply.github.com>
2019-02-21 10:19:37 +0100

    Update Cat class
    aaaaaaaa

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public interface IAnimal
+    {
+        string GetDescription();
+    }
+
+    public interface IHealth
+    {
+        string GetHealth();
+    }
+
+    class Cat : IAnimal
+    {
+        public string GetDescription()
+        {
+            return "I'm a cat";
+        }
+    }
+
+    class Soldier : IHealth
+    {
+        public string GetHealth()
+        {
+            return "I am healthy.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+
+            Soldier mySoldier = new Soldier();
+
+            Console.WriteLine(myCat.GetDescription());
+
+            Console.WriteLine(mySoldier.GetHealth());
+
+            Console.Write("Press Enter to continue!");
+            Console.ReadLine();
+        }
+    }
+
+}
